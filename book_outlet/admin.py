0ec37f026fe343fb30a7a52f4e8fd1fc9c2f7960
massimo-briceno <massimobriceno@gmail.com>
2024-01-22 12:27:54 +0100

    list filter and display set

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -4,6 +4,8 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating", "is_bestselling")
+    list_display = ("title", "author", "rating", "is_bestselling")
 
 
 # Register your models here.
